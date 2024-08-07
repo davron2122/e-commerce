df4a092da14b7791607122fa22f44a94940c1f06
Davron <eliboyevdavron2122@gmail.com>
2024-08-07 14:52:19 +0900

    created activity_main.xml and add some changes to MainViewModel.kt

--- app/src/main/java/com/example/e_commerce/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce/presentation/main/MainActivity.kt
@@ -18,5 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
         }
     }
--- app/src/main/java/com/example/e_commerce/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/example/e_commerce/presentation/main/MainViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.e_commerce.presentation.main
 
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 @HiltViewModel
-class MainViewModel @Inject constructor() {
+class MainViewModel @Inject constructor(
+
+) : ViewModel(){
 
 
 
