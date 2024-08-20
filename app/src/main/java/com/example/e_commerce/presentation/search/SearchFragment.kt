82f9da71d6fc3fa27951b50baba26d83d2fb3596
Davron <eliboyevdavron2122@gmail.com>
2024-08-20 10:10:47 +0900

    created fragment_search.xml, SearchFragment.kt

--- app/src/main/java/com/example/e_commerce/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/example/e_commerce/presentation/search/SearchFragment.kt
@@ -1,6 +1,15 @@
 package com.example.e_commerce.presentation.search
 
+import androidx.fragment.app.viewModels
+import com.example.e_commerce.databinding.FragmentSearchBinding
 import com.example.e_commerce.util.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class SearchFragment  : BaseFragment{
+@AndroidEntryPoint
+class SearchFragment  : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::inflate){
+
+    private val viewModel by viewModels<SearchViewModel> {  }
+
+
+
 }
