67e72194e672e8fbc2b7462c93313b942556b1d1
Davron <eliboyevdavron2122@gmail.com>
2024-08-21 15:08:19 +0900

    added CategoriesViewModel.kt, DetailFragment.kt

---
+++ app/src/main/java/com/example/e_commerce/presentation/categories/CategoriesViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.e_commerce.presentation.categories
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.e_commerce.data.api.product.dto.Category
+import com.example.e_commerce.domain.repo.ProductRepository
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CategoriesViewModel @Inject constructor(
+    private val productRepository: ProductRepository
+) : ViewModel() {
+
+    val loading = MutableLiveData(false)
+    val error = MutableLiveData(false)
+    val categories = MutableLiveData<List<Category>>()
+
+    /*
+     it calls the getCategories() function to fetch the list of
+     categories when the ViewModel is created.
+     */
+    init {
+        getCategories()
+    }
+
+    fun getCategories() = viewModelScope.launch {
+        loading.postValue(true)
+        error.postValue(false)
+        try {
+            val response = productRepository.getCategories()
+            categories.postValue(response)
+        } catch (e: Exception) {
+            error.postValue(true)
+        } finally {
+            loading.postValue(false)
+        }
+    }
+}
